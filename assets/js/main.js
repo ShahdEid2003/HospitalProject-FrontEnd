6550e4318ed899088d4137ecf97a16dc5e5cdbbe
ShahdEid2003 <shahdeid012@gmail.com>
2024-11-03 00:15:55 +0200

    first-commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+// const navItem1= document.querySelector('.navbar-item1');
+//   const homeContent = document.querySelector('.home-content');
+
+//   // Show dropdown on mouse enter
+//   navItem1.addEventListener('mouseenter', () => {
+//     homeContent.style.display = 'block';
+//   });
+
+//   // Hide dropdown on mouse leave
+//   navItem1.addEventListener('mouseleave', () => {
+//     homeContent.style.display = 'none';
+//   });
