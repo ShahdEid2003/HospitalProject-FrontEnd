36c5bb77b8fb0dc945b88bbeefc259c7747bc997
ShahdEid2003 <shahdeid012@gmail.com>
2024-11-04 21:34:37 +0200

    swiper medical services

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,54 @@
 
-// const navItem1= document.querySelector('.navbar-item1');
-//   const homeContent = document.querySelector('.home-content');
+const images = document.querySelectorAll('.carousel-item .image');
+const zoomButtonNext = document.querySelector('.carousel-control-next');
+const zoomButtonPre = document.querySelector('.carousel-control-prev');
+const animations = ['glitch', 'zoom-in', 'fade-in','slide-in'];
 
-//   // Show dropdown on mouse enter
-//   navItem1.addEventListener('mouseenter', () => {
-//     homeContent.style.display = 'block';
-//   });
 
-//   // Hide dropdown on mouse leave
-//   navItem1.addEventListener('mouseleave', () => {
-//     homeContent.style.display = 'none';
-//   });
+zoomButtonNext.addEventListener('click', () => {
+    images.forEach(image => {
+     
+        animations.forEach(animation => image.classList.remove(animation));
+        
+        const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+
+        image.classList.add(randomAnimation);
+    });
+});
+zoomButtonPre.addEventListener('click', () => {
+    images.forEach(image => {
+     
+        animations.forEach(animation => image.classList.remove(animation));
+        
+        const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+
+        image.classList.add(randomAnimation);
+    });
+});
+
+const swiper = new Swiper('.swiper-container', {
+    spaceBetween: 7,
+    freeMode: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+     
+      640: {
+        slidesPerView: 1,
+      },
+      
+      768: {
+        slidesPerView: 2,
+      },
+     
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+ 
+  
+  
